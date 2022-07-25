369f3edf0150a6b8c0f4d860ede9ee72478ca4c1
ef-mark.o <olivaresmarkanthony14@outlook.com>
2022-07-25 10:05:09 +0800

    Done with the SERVICE LAYER

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,7 @@ create table book
     primary key(id)
 );
 
-# insert into book values (1 , '6-543-1798-5', 'Programming 1', 'Janus Tan', 'This is my description1', 'Programming');
+# insert into book values (1 , '6-543-1798-5', 'Programming 1', 'Janus Tan', 'This is my description1', 'Programming') ;
 # insert into book values (2, '1-123-3456-3', 'Data Structure', 'Janus Tan', 'This is my description2', 'Programming');
 # insert into book values (3, '7-645-7723-4', 'Human Computer Interaction', 'Janus Tan', 'This is my description3', 'Programming');
 # select * from book;
